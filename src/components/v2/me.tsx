c3275232b930f1adab5ef3f28651d9aa2dcb207a
Anthony Mensah <anthonymensah1030@gmail.com>
2025-12-12 14:09:48 +0100

    about me image updated

--- src/components/v2/me.tsx
+++ src/components/v2/me.tsx
@@ -30,7 +30,7 @@ const Me = () => {
         <main className="container mx-auto flex flex-col items-center space-y-3 md:flex-row md:justify-center md:space-x-10">
           <div className="justify-end md:flex md:h-[500px] md:flex-1">
             <img
-              src="saint.jpg"
+              src="saintt.jpg"
               alt="Anthony Mensah - Frontend Developer"
               className="aspect-square rounded-3xl object-cover"
             />
